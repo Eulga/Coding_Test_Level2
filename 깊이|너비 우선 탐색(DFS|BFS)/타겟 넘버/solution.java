8b36faf8a6bc6198305ceae1b8ed07ec2092d898
Eulga <63146118+Eulga@users.noreply.github.com>
2022-03-08 14:01:35 +0900

    Create solution.java

---
+++ 깊이|너비 우선 탐색(DFS|BFS)/타겟 넘버/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        int current = numbers[0];
+        
+        answer += dfs(current, 1, numbers, target);
+        answer += dfs(-current, 1, numbers, target);
+        return answer;
+    }
+    
+    public int dfs(int prev, int index, int[] numbers, int target) {
+        if(index >= numbers.length) {
+            if(prev == target) {
+                return 1;
+            }
+            return 0;
+        }
+        
+        int cur1 = prev + numbers[index];
+        int cur2 = prev - numbers[index];
+        
+        int ans = 0;
+        ans += dfs(cur1, index+1, numbers, target);
+        ans += dfs(cur2, index+1, numbers, target);
+        
+        return ans;
+    }
+}
